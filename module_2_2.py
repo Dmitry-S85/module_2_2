0d9fabb675d5c3678cf1de08356860af2e7e5ea7
Dmitry-S85 <dmitry.stoyankin@yandex.ru>
2024-06-25 23:22:58 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,15 @@
+first = 1
+second = 2
+third = 3
+
+if first == second == third:
+    print(3)
+elif first == second:
+    print(2)
+elif second == third:
+    print(2)
+elif first == third:
+    print(2)
+else:
+    first != second != third
+    print(0)
